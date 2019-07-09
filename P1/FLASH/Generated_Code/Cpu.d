fef75a8472c373c0c664102a14471ab88e2d638a
HoaiBaoGT <1410239@hcmut.edu.vn>
2019-07-09 14:55:07 +0700

    Project 1

---
+++ P1/FLASH/Generated_Code/Cpu.d
@@ -0,0 +1,162 @@
+Generated_Code/Cpu.o: ../Generated_Code/Cpu.c \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ ../Generated_Code/TSS1.h ../Generated_Code/PE_Types.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ ../Generated_Code/PE_Error.h ../Generated_Code/PE_Const.h \
+ ../Generated_Code/IO_Map.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/iofiles/MKL46Z4.h \
+ ../Generated_Code/Cpu.h D:/CW/P1/Sources/TSS/TSS_API.h \
+ D:/CW/P1/Sources/TSS_SystemSetup.h \
+ D:/CW/P1/Sources/TSS/TSS_StatusCodes.h \
+ D:/CW/P1/Sources/TSS/TSS_DataTypes.h D:/CW/P1/Sources/TSS/TSS_Sensor.h \
+ D:/CW/P1/Sources/derivative.h D:/CW/P1/Generated_Code/Cpu.h \
+ D:/CW/P1/Sources/TSS/TSS_API.h ../Generated_Code/PWM.h \
+ ../Generated_Code/PwmLdd1.h ../Generated_Code/TU1.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/TPM_PDD.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h \
+ ../Generated_Code/Console.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdio \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstddef \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/null.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/size_t.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/wchar_t.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_rsize_t.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/eof.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/va_list.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/file_struc.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio_api.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdarg.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdarg \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/stdarg.ARM.h \
+ ../Generated_Code/IO1.h D:/CW/P1/Sources/Events.h \
+ D:/CW/P1/Generated_Code/PE_Types.h D:/CW/P1/Generated_Code/PE_Error.h \
+ D:/CW/P1/Generated_Code/PE_Const.h D:/CW/P1/Generated_Code/IO_Map.h \
+ D:/CW/P1/Generated_Code/TSS1.h D:/CW/P1/Generated_Code/PWM.h \
+ D:/CW/P1/Generated_Code/PwmLdd1.h D:/CW/P1/Generated_Code/TU1.h \
+ D:/CW/P1/Generated_Code/Console.h D:/CW/P1/Generated_Code/IO1.h
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+../Generated_Code/TSS1.h:
+
+../Generated_Code/PE_Types.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/IO_Map.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/iofiles/MKL46Z4.h:
+
+../Generated_Code/Cpu.h:
+
+D:/CW/P1/Sources/TSS/TSS_API.h:
+
+D:/CW/P1/Sources/TSS_SystemSetup.h:
+
+D:/CW/P1/Sources/TSS/TSS_StatusCodes.h:
+
+D:/CW/P1/Sources/TSS/TSS_DataTypes.h:
+
+D:/CW/P1/Sources/TSS/TSS_Sensor.h:
+
+D:/CW/P1/Sources/derivative.h:
+
+D:/CW/P1/Generated_Code/Cpu.h:
+
+D:/CW/P1/Sources/TSS/TSS_API.h:
+
+../Generated_Code/PWM.h:
+
+../Generated_Code/PwmLdd1.h:
+
+../Generated_Code/TU1.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/TPM_PDD.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h:
+
+../Generated_Code/Console.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdio:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstddef:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/null.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/size_t.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/wchar_t.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_rsize_t.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/eof.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/va_list.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/file_struc.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio_api.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdarg.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdarg:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/stdarg.ARM.h:
+
+../Generated_Code/IO1.h:
+
+D:/CW/P1/Sources/Events.h:
+
+D:/CW/P1/Generated_Code/PE_Types.h:
+
+D:/CW/P1/Generated_Code/PE_Error.h:
+
+D:/CW/P1/Generated_Code/PE_Const.h:
+
+D:/CW/P1/Generated_Code/IO_Map.h:
+
+D:/CW/P1/Generated_Code/TSS1.h:
+
+D:/CW/P1/Generated_Code/PWM.h:
+
+D:/CW/P1/Generated_Code/PwmLdd1.h:
+
+D:/CW/P1/Generated_Code/TU1.h:
+
+D:/CW/P1/Generated_Code/Console.h:
+
+D:/CW/P1/Generated_Code/IO1.h:
